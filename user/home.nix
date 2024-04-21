9aea42657ccc42ee504cbbdb952df0ce9b9ce64e
Bart Vegter <mail@bartvegter.com>
2024-04-22 00:05:01 +0200

    Updated modules for testing

--- user/home.nix
+++ user/home.nix
@@ -2,42 +2,32 @@
 
 {
   imports = [
-    # ./hyprland.nix
     ./sh.nix
+    ./git.nix
+    # ./hyprland.nix
+    ./file-links.nix
+    ./fonts.nix
     ./theme.nix
+    # ./syncthing.nix
   ];
 
   # Needed by home manager
+  programs.home-manager.enable = true;
+
   home = {
     username = userSettings.username;
     homeDirectory = "/home/" + userSettings.username;
     stateVersion = "23.11"; # Only edit this if you know what you're doing
   };
-  programs.home-manager.enable = true;
 
-  # Git
-  programs.git = {
-    enable = true;
-    userName = userSettings.name;
-    userEmail = userSettings.email;
-    extraConfig = {
-      init.defaultBranch = "main";
-    };
+  home.sessionVariables = {
+    EDITOR = userSettings.editor;
+    TERM = userSettings.term;
   };
 
   # Enables ssh-agent for user.
   services.ssh-agent.enable = true;
   programs.ssh.addKeysToAgent = "ask";
-
-  # Syncthing
-  services.syncthing = {
-    package = pkgs.syncthing;
-    enable = true;
-    tray.enable = true;
-  };
-
-  # Makes fonts installed in home.nix discoverable
-  fonts.fontconfig.enable = true;
 
   # User defined packages
   nixpkgs.config.allowUnfree = true;
@@ -68,11 +58,6 @@
       libgnome-keyring
       libreoffice-fresh
       neovim
-      noto-fonts
-      noto-fonts-lgc-plus
-      noto-fonts-cjk-serif
-      noto-fonts-color-emoji
-      (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
       nsxiv
       nwg-look
       obs-studio
@@ -109,73 +94,4 @@
     (with pkgs-stable; [
       # If any package breaks, try installing the stable version here.
     ]);
-
-  home.file = {
-    ".config/alacritty/" = {
-      source = ./dotfiles/.config/alacritty;
-      recursive = true;
-    };
-    ".config/btop/" = {
-      source = ./dotfiles/.config/btop;
-      recursive = true;
-    };
-    ".config/color-scheme/" = {
-      source = ./dotfiles/.config/color-scheme;
-      recursive = true;
-    };
-    ".config/homepage/" = {
-      source = ./dotfiles/.config/homepage;
-      recursive = true;
-    };
-    ".config/htop/" = {
-      source = ./dotfiles/.config/htop;
-      recursive = true;
-    };
-    ".config/hypr/" = {
-      source = ./dotfiles/.config/hypr;
-      recursive = true;
-    };
-    ".config/mako/" = {
-      source = ./dotfiles/.config/mako;
-      recursive = true;
-    };
-    # ".config/nvim/" = {
-    #   source = ./dotfiles/.config/nvim;
-    #   recursive = true;
-    # };
-    ".config/starship/" = {
-      source = ./dotfiles/.config/starship;
-      recursive = true;
-    };
-    ".config/Thunar/" = {
-      source = ./dotfiles/.config/Thunar;
-      recursive = true;
-    };
-    ".config/tofi/" = {
-      source = ./dotfiles/.config/tofi;
-      recursive = true;
-    };
-    ".config/waybar/" = {
-      source = ./dotfiles/.config/waybar;
-      recursive = true;
-    };
-    ".config/wlogout/" = {
-      source = ./dotfiles/.config/wlogout;
-      recursive = true;
-    };
-    # gtk-2,3,4
-    # xsettingsd
-
-    ".local/bin/" = {
-      source = ./dotfiles/.local/bin;
-      recursive = true;
-    };
-    # .icons
-    # .themes
-  };
-
-  home.sessionVariables = {
-    EDITOR = userSettings.editor;
-    TERM = userSettings.term;
-  };
 }
--- user/hyprland.nix
+++ user/hyprland.nix
@@ -1,9 +1,15 @@
-{ config, pkgs, ... }:
+{ config, pkgs, userSettings, ... }:
 
 {
+  imports = [
+    ./fonts.nix
+    ./theme.nix
+  ];
+
   wayland.windowManager.hyprland = {
     enable = true;
     systemd.enable = true;
   };
+
   programs.waybar.enable = true;
 }
