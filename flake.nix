42a6ca76841390f980cc884f3aba9fd177d39d86
Bart Vegter <mail@bartvegter.com>
2024-04-22 19:19:40 +0200

    Fixed typo in flake.nix

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
         version = "unstable";
         system = "x86_64-linux";
         hostname = "hyprnix";
-        dotfilesPath = "~/." + ${hostname};
+        dotfilesPath = "~/." + hostname;
         bootMode = "uefi";
         bootMountPath = "/boot";
         grubDevice = "";
