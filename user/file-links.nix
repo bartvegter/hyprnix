e2ed79c6aeab123c8cbcbd91d5aa80f747ffa24a
Bart Vegter <mail@bartvegter.com>
2024-04-22 18:58:14 +0200

    Moved dotfiles directory to user && Deleted old .icons & .themes directories

--- user/file-links.nix
+++ user/file-links.nix
@@ -3,62 +3,62 @@
 {
   home.file = {
     ".config/alacritty/" = {
-      source = ../dotfiles/.config/alacritty;
+      source = ./dotfiles/.config/alacritty;
       recursive = true;
     };
     ".config/btop/" = {
-      source = ../dotfiles/.config/btop;
+      source = ./dotfiles/.config/btop;
       recursive = true;
     };
     ".config/color-scheme/" = {
-      source = ../dotfiles/.config/color-scheme;
+      source = ./dotfiles/.config/color-scheme;
       recursive = true;
     };
     ".config/homepage/" = {
-      source = ../dotfiles/.config/homepage;
+      source = ./dotfiles/.config/homepage;
       recursive = true;
     };
     ".config/htop/" = {
-      source = ../dotfiles/.config/htop;
+      source = ./dotfiles/.config/htop;
       recursive = true;
     };
     ".config/hypr/" = {
-      source = ../dotfiles/.config/hypr;
+      source = ./dotfiles/.config/hypr;
       recursive = true;
     };
     ".config/mako/" = {
-      source = ../dotfiles/.config/mako;
+      source = ./dotfiles/.config/mako;
       recursive = true;
     };
     # ".config/nvim/" = {
-    #   source = ../dotfiles/.config/nvim;
+    #   source = ./dotfiles/.config/nvim;
     #   recursive = true;
     # };
     ".config/starship/" = {
-      source = ../dotfiles/.config/starship;
+      source = ./dotfiles/.config/starship;
       recursive = true;
     };
     ".config/Thunar/" = {
-      source = ../dotfiles/.config/Thunar;
+      source = ./dotfiles/.config/Thunar;
       recursive = true;
     };
     ".config/tofi/" = {
-      source = ../dotfiles/.config/tofi;
+      source = ./dotfiles/.config/tofi;
       recursive = true;
     };
     ".config/waybar/" = {
-      source = ../dotfiles/.config/waybar;
+      source = ./dotfiles/.config/waybar;
       recursive = true;
     };
     ".config/wlogout/" = {
-      source = ../dotfiles/.config/wlogout;
+      source = ./dotfiles/.config/wlogout;
       recursive = true;
     };
     # gtk-2,3,4
     # xsettingsd
 
     ".local/bin/" = {
-      source = ../dotfiles/.local/bin;
+      source = ./dotfiles/.local/bin;
       recursive = true;
     };
     # .icons
