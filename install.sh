32b1362489eee3657f7cd55b2caa2916cbf2e967
Bart Vegter <mail@bartvegter.com>
2024-04-22 18:48:09 +0200

    Simplified some if-statements to cases

--- install.sh
+++ install.sh
@@ -23,19 +23,15 @@ gitAdd
 
 # Set UEFI or BIOS bootMode
 if [ -d /sys/firmware/efi/efivars ]; then
-
     sed -i "0,/bootMode.*=.*\".*\";/s//bootMode = \"uefi\";/" $SCRIPT_DIR/flake.nix
     echo && echo ":: Detected UEFI compatibility"
     echo ":: Setting bootMode to UEFI..." && echo
-
 else
-
     sed -i "0,/bootMode.*=.*\".*\";/s//bootMode = \"bios\";/" $SCRIPT_DIR/flake.nix
     grubDevice=$(findmnt / | awk -F' ' '{ print $2 }' | sed 's/\[.*\]//g' | tail -n 1 | lsblk -no pkname | tail -n 1 )
     sed -i "0,/grubDevice.*=.*\".*\";/s//grubDevice = \"\/dev\/$grubDevice\";/" $SCRIPT_DIR/flake.nix
     echo && echo ":: Could not detect UEFI compatibility"
     echo ":: Setting bootMode to BIOS (legacy)..." && echo
-
 fi
 gitAdd
 
@@ -43,81 +39,97 @@ gitAdd
 echo
 while true; do
   read -p ":: Would you like to change the default configuration? (recommended) [Y/n]: " ynConf
-  if [ -z $ynConf ] || [ $ynConf == "Y" ] || [ $ynConf == "y" ]; then
+  case $ynConf in
+    -z | "Y" | "y")
 
-    echo
-    while true; do
-      read -p ":: (1/5) Please enter your username as in /home/{username}/ (ex. 'bart'): " username
-      if [ -z $username ]; then
-        echo ":: Username cannot be empty, try again..." && echo
-      else
-        break
-      fi
-    done
+      echo
+      while true; do
+        read -p ":: (1/5) Please enter your username as in /home/{username}/ (ex. 'bart'): " username
+        if [ -z $username ]; then
+          echo ":: Username cannot be empty, try again..." && echo
+        else
+          break
+        fi
+      done
 
-    echo
-    while true; do
-      read -p ":: (2/5) Please enter your name for user description and git commits (ex. 'Bart'): " name
-      if [ -z $name ]; then
-        echo ":: Name cannot be empty, try again..." && echo
-      else
-        break
-      fi
-    done
+      echo
+      while true; do
+        read -p ":: (2/5) Please enter your name for user description and git commits (ex. 'Bart'): " name
+        if [ -z $name ]; then
+          echo ":: Name cannot be empty, try again..." && echo
+        else
+          break
+        fi
+      done
 
-    echo && read -p ":: (3/5) Please enter your email address for git commits (optional, ex. 'example@example.com'): " email
+      echo && read -p ":: (3/5) Please enter your email address for git commits (optional, ex. 'example@example.com'): " email
 
-    echo
-    while true; do
-      echo && read -p ":: (4/5) Would you like to use zsh as your default shell, instead of bash? [Y/n]: " ynShell
-      if [ -z $ynShell ] || [ $ynShell == "Y" ] || [ $ynShell == "y" ]; then
-        break
-      elif [ $ynShell == "N" ] || [ $ynShell == "n" ]; then
-        sed -i "0,/zsh/s//bashInteractive/" $SCRIPT_DIR/flake.nix
-        break
-      else
-        echo ":: Invalid input, please try again..." && echo
-        echo ":: Valid values are [Y] or [n] (case insensitive), or press [return] for default (Y)"
-      fi
-    done
+      echo
+      while true; do
+        echo && read -p ":: (4/5) Would you like to use zsh as your default shell, instead of bash? [Y/n]: " ynShell
+        case $ynShell in
+          -z | "Y" | "y")
+            break
+            ;;
 
-    echo
-    while true; do
-      echo && read -p ":: (5/5) Please choose a default editor [neovim|vim|nano|vscode/vscodium(will use nano in terminal)]: " editor
-      case $editor in
-        "neovim" | "nvim")
-          break;;
-        "vim" | "nano")
-          sed -i "0,/neovim/s//$editor/" $SCRIPT_DIR/flake.nix
-          break;;
-        "vscode" | "vscodium")
-          sed -i "0,/neovim/s//$editor/" $SCRIPT_DIR/flake.nix
-          $editor = nano
-          break;;
-        * )
+          "N" | "n")
+            sed -i "0,/zsh/s//bashInteractive/" $SCRIPT_DIR/flake.nix
+            break
+            ;;
+
+          *)
           echo ":: Invalid input, please try again..." && echo
-          echo ":: Valid values are listed in between [],in lower case"
-      esac
-    done
+          echo ":: Valid values are [Y] or [n] (case insensitive), or press [return] for default (Y)"
+          ;;
+        esac
+      done
 
-    echo && echo ":: Applying user settings..." && echo
-    sed -i "0,/bart/s//$username/" $SCRIPT_DIR/flake.nix
-    sed -i "0,/Bart/s//$name/" $SCRIPT_DIR/flake.nix
-    sed -i "0,/contact@bartvegter.com/s//$email/" $SCRIPT_DIR/flake.nix
-    break
+      echo
+      while true; do
+        echo && read -p ":: (5/5) Please choose a default editor [neovim|vim|nano|vscode/vscodium(will use nano in terminal)]: " editor
+        case $editor in
+          "neovim" | "nvim")
+            break
+            ;;
 
-  elif [ $ynConf == "N" ] || [ $ynConf == "n" ]; then
+          "vim" | "nano")
+            sed -i "0,/neovim/s//$editor/" $SCRIPT_DIR/flake.nix
+            break
+            ;;
 
-    echo && echo ":: Setting system defaults..." && echo
-    sed -i "0,/bart/s//$(whoami)/" $SCRIPT_DIR/flake.nix
-    sed -i "0,/Bart/s//$(getent passwd $(whoami) | cut -d ':' -f 5 | cut -d ',' -f 1)/" $SCRIPT_DIR/flake.nix
-    sed -i "s/contact@bartvegter.com//" $SCRIPT_DIR/flake.nix
-    break
+          "vscode" | "vscodium")
+            sed -i "0,/neovim/s//$editor/" $SCRIPT_DIR/flake.nix
+            $editor = nano
+            break
+            ;;
 
-  else
-    echo ":: Invalid input, please try again..." && echo
-    echo ":: Valid values are [Y] or [n] (case insensitive), or press [return] for default (Y)"
-  fi
+          *)
+            echo ":: Invalid input, please try again..." && echo
+            echo ":: Valid values are listed below in between [], enter value in lower case"
+            ;;
+        esac
+      done
+
+      echo && echo ":: Applying user settings..." && echo
+      sed -i "0,/bart/s//$username/" $SCRIPT_DIR/flake.nix
+      sed -i "0,/Bart/s//$name/" $SCRIPT_DIR/flake.nix
+      sed -i "0,/contact@bartvegter.com/s//$email/" $SCRIPT_DIR/flake.nix
+      break
+      ;;
+
+    "N" | "n")
+      echo && echo ":: Setting system defaults..." && echo
+      sed -i "0,/bart/s//$(whoami)/" $SCRIPT_DIR/flake.nix
+      sed -i "0,/Bart/s//$(getent passwd $(whoami) | cut -d ':' -f 5 | cut -d ',' -f 1)/" $SCRIPT_DIR/flake.nix
+      sed -i "s/contact@bartvegter.com//" $SCRIPT_DIR/flake.nix
+      break
+      ;;
+
+    *)
+      echo ":: Invalid input, please try again..." && echo
+      echo ":: Valid values are [Y] or [n] (case insensitive), or press [return] for default (Y)"
+      ;;
+  esac
 done
 gitAdd
 
@@ -128,16 +140,22 @@ fi
 # Open up editor to manually edit flake.nix before install
 while true; do
   read -p ":: Would you like to manually edit flake.nix for further configuration? [y/N]: " ynManual
-  if [ $ynManual == "Y" ] || [ $ynManual == "y" ]; then
-    $EDITOR $SCRIPT_DIR/flake.nix;
-    gitAdd
-    break
-  elif [ -z $ynManual ] || [ $ynManual == "N" ] || [ $ynManual == "n" ]; then
-    break
-  else
-    echo ":: Invalid input, please try again..." && echo
-    echo ":: Valid values are [y] or [N] (case insensitive), or press [return] for default (N)"
-  fi
+  case $ynManual in
+    "Y" | "y")
+      $EDITOR $SCRIPT_DIR/flake.nix;
+      gitAdd
+      break
+      ;;
+
+    -z | "N" | "n")
+      break
+      ;;
+
+    *)
+      echo ":: Invalid input, please try again..." && echo
+      echo ":: Valid values are [y] or [N] (case insensitive), or press [return] for default (N)"
+      ;;
+  esac
 done
 
 # Permissions for files that should be owned by root
@@ -159,15 +177,21 @@ echo && echo ":: Hyprnix installed successfully" && echo
 while true; do
   read -p ":: Would you like to reboot into Hyprland? [Y/n]: " ynReboot
 
-  if [ -z $ynReboot ] || [ $ynReboot == "Y" ] || [ $ynReboot == "y" ]; then
-    echo ":: Rebooting system..."
-    systemctl reboot
-    exit
-  elif [ $ynReboot == "N" ] || [ $ynReboot == "n" ]; then
-    echo ":: When ready, run \'systemctl reboot\' to reboot into Hyprland"
-    exit
-  else
-    echo ":: Invalid input, please try again..."
-  fi
+  case $ynReboot in
+    -z | "Y" | "y")
+      echo && echo ":: Rebooting system..." && echo
+      systemctl reboot
+      exit
+      ;;
 
+    "N" | "n")
+      echo && echo ":: When ready, run \'systemctl reboot\' to reboot into Hyprland" && echo
+      exit
+      ;;
+
+    *)
+      echo ":: Invalid input, please try again..." && echo
+      echo ":: Valid values are [Y] or [n] (case insensitive), or press [return] for default (Y)"
+      ;;
+  esac
 done
