6dbec1d4f859569fffc457b251877e42388b1d56
Bart <contact@bartvegter.com>
2024-04-24 23:21:42 +0200

    Fixed relative path to dotfiles directory

--- modules/home/app/nvim.nix
+++ modules/home/app/nvim.nix
@@ -9,7 +9,7 @@
 
     programs.neovim = {
       enable = true;
-      extraConfig = lib.fileContents ./dotfiles/.config/nvim/init.lua;
+      extraConfig = lib.fileContents ../hyprland/dotfiles/.config/nvim/init.lua;
     };
 
     home.packages = with pkgs; [
