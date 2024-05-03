3adbef7ad052c93c2e1bb6e17301397efcd5da08
Bart <contact@bartvegter.com>
2024-05-03 02:32:21 +0200

    Fixed typo in ncg.sh

--- bin/ncg.sh
+++ bin/ncg.sh
@@ -3,7 +3,7 @@
 if [[ $1 == "-a" ]] || [[ $1 == "--all" ]]; then
   echo && echo ":: Deleting all old profiles and store objects" && echo
   sudo nix-collect-garbage -d
-  echo && echo ":: Cleaning up boot old entries" && echo
+  echo && echo ":: Cleaning up old boot entries" && echo
   sudo nix-env --delete-generations old --profile /nix/var/nix/profiles/system \
   && sudo /nix/var/nix/profiles/system/bin/switch-to-configuration switch
 else
