cc7a6dc6e1da4d89b8dcefc7fdce8c61fee0aa1a
Bart <contact@bartvegter.com>
2024-05-06 01:43:57 +0200

    Disabled WLR_NO_HARDWARE_CURSORS in Hyprland session variables, which caused some games to improperly register the pointer location

--- modules/system/hyprland/hyprland.nix
+++ modules/system/hyprland/hyprland.nix
@@ -17,7 +17,7 @@
     programs.seahorse.enable = true;
 
     environment.sessionVariables = {
-      WLR_NO_HARDWARE_CURSORS = "1";
+      # WLR_NO_HARDWARE_CURSORS = "1";
       NIXOS_OZONE_WL = "1";
     };
 
