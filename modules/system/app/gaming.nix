54bc1f7fed756f6ca0c2f5c89e1f4a1901ff76ab
Bart <contact@bartvegter.com>
2024-06-19 13:56:01 +0200

    Added mangohud config

--- modules/system/app/gaming.nix
+++ modules/system/app/gaming.nix
@@ -14,11 +14,67 @@
     environment.systemPackages = with pkgs; [
       bottles
       lutris
-      mangohud
       protonup
     ];
 
     programs.gamemode.enable = true;
+
+    programs.mangohud = {
+      enable = true;
+      settings = {
+        gpu_stats;
+        gpu_temp;
+        # gpu_junction_temp
+        # gpu_core_clock
+        # gpu_mem_temp
+        # gpu_mem_clock
+        # gpu_fan
+        # gpu_voltage
+
+        # Display the current CPU information
+        cpu_stats;
+        cpu_temp;
+        cpu_mhz;
+        # cpu_power
+        # core_load
+        # core_load_change
+
+        vram;
+        ram;
+
+        fps;
+        frametime;
+        frame_timing;
+        # histogram;
+
+        throttling_status;
+
+        gpu_name;
+        vulkan_driver;
+        gamemode;
+
+        ### Gamescope related options
+        ## Display the status of FSR (only works in gamescope)
+        # fsr
+        ## Hides the sharpness info for the `fsr` option (only available in gamescope)
+        # hide_fsr_sharpness
+        ## Shows the graph of gamescope app frametimes and latency (only on gamescope obviously)
+        # debug
+        ## Display the status of HDR (only works in gamescope)
+        # hdr
+        ## Display the current refresh rate (only works in gamescope)
+        # refresh_rate
+
+
+        # font_size=24
+        # font_scale=1.0
+        # font_size_text=24
+        # font_scale_media_player=0.55
+        # no_small_font
+        # font_file=
+        # text_outline;
+      };
+    };
 
     programs.steam = {
       enable = true;
