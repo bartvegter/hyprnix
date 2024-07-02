f9251fa82d6c828e4e1940d88207ef21e51f6717
Bart <contact@bartvegter.com>
2024-07-02 22:19:34 +0200

    Set workspace 4 as default for DP-3

--- modules/home/hyprland/hyprland.nix
+++ modules/home/hyprland/hyprland.nix
@@ -234,7 +234,7 @@
 
         workspace = [
           "1, defaultName: brave"
-          "4, defaultName: gaming"
+          "4, defaultName: gaming, monitor:DP-3, default:true"
           "5, defaultName: activeGame"
           "6, defaultName: spotify, on-created-empty: [fullscreen] spotify"
         ];
